484609b55ec7c6535397963fdda0b72307783d4c
claudia <zapatamoralesclaudia@gmail.com>
2022-11-12 11:19:00 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,11 @@
+// const alumnos =["jose",  "jorge" , "juan"];
+
+// module.exports=alumnos;
+
+export const alumnos =["jose",  "jorge" , "juan"];
+
+export const getAlumnos=()=>{
+    console.log("obtener alumnos")
+}
+
+
---
+++ index.js
@@ -0,0 +1,10 @@
+// const dataAlumnos = requore("./data");
+// // console.log(dataAlumnos);
+// import alumnos from "./data";
+
+import { alumnos, getAlumnos } from "./data.js";
+
+ console.log(alumnos) 
+
+
+getAlumnos()
